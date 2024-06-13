7d3e51c11ac24820b0f8e987443787a7ccdd38f6
thinnweoo <89743228+thinnweoo@users.noreply.github.com>
2024-06-13 16:21:43 +0700

    Add files via upload

---
+++ SquareHollowPattern.java
@@ -0,0 +1,29 @@
+package assignments;
+import java.util.Scanner;
+
+public class SquareHollowPattern {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+//		System.out.println("enter your size");
+//		int size = s.nextInt();
+		int size = 5;
+		for(int i=0; i<=size; i++) {
+			for(int j=0; j<size; j++) {		//j<=size
+				if(i == 0 || i == size || j == 0 || j == size-1) {		//j==size
+					System.out.print("* ");
+				}else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		
+		s.close();
+
+	}
+
+}
